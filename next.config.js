35d29707c2381b42874b41fe4589bf31b5e67ff2
fahernkhan <fathurrahmanhernanda123@gmail.com>
2025-05-16 23:27:10 +0700

    remove sentry

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'utfs.io' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'platform-lookaside.fbsbx.com' },
+    ],
+  },
+}
+
+module.exports = nextConfig
